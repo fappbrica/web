9edb55bf8b3cb359bb3d38702e12833b8c8c25f6
AxelCastr00 <axelfabcastro27@gmail.com>
2023-10-07 13:45:44 -0300

    text and logos were added, we took out submenus

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
-import Link from "next/link";
-import appData from "../../data/app.json";
-import { handleDropdown, handleMobileDropdown } from "../../common/navbar";
+import React from 'react';
+import Link from 'next/link';
+import appData from '../../data/app.json';
+import { handleDropdown, handleMobileDropdown } from '../../common/navbar';
 
 const Navbar = ({ lr, nr, theme }) => {
   return (
     <nav
       ref={nr}
       className={`navbar navbar-expand-lg change ${
-        theme === "themeL" ? "light" : ""
+        theme === 'themeL' ? 'light' : ''
       }`}
     >
       <div className="container">
         <Link href="/">
           <a className="logo">
             {theme ? (
-              theme === "themeL" ? (
+              theme === 'themeL' ? (
                 <img ref={lr} src={appData.darkLogo} alt="logo" />
               ) : (
                 <img ref={lr} src={appData.lightLogo} alt="logo" />
@@ -44,78 +44,21 @@ const Navbar = ({ lr, nr, theme }) => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Inicio
-              </span>
-              <div className="dropdown-menu">
-                <Link href={`/homepage/home1-dark`}>
-                  <a className="dropdown-item">Página principal</a>
-                </Link>
-                <Link href={`/homepage/home2-dark`}>
-                  <a className="dropdown-item">Agencia creativa</a>
-                </Link>
-                <Link href={`/homepage/home5-dark`}>
-                  <a className="dropdown-item">Agencia digital</a>
-                </Link>
-                <Link href={`/homepage/home4-dark`}>
-                  <a className="dropdown-item">Seccion de negocios</a>
-                </Link>
-                <Link href={`/homepage/home3-dark`}>
-                  <a className="dropdown-item">Empresas</a>
-                </Link>
-                <Link href={`/homepage/home6-dark`}>
-                  <a className="dropdown-item">Agencia moderna</a>
-                </Link>
-                <Link href={`/homepage/home7-dark`}>
-                  <a className="dropdown-item">Freelancer</a>
-                </Link>
-                <Link href={`/homepage/home8-dark`}>
-                  <a className="dropdown-item">Arquitectura</a>
-                </Link>
-              </div>
+            <li className="nav-item">
+              <Link href={`/homepage/home5-dark`}>
+                <a className="nav-link">Inicio</a>
+              </Link>
             </li>
+
             <li className="nav-item">
               <Link href={`/about/about-dark`}>
                 <a className="nav-link">Somos</a>
               </Link>
             </li>
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Nuestros proyectos
-              </span>
-              <div className="dropdown-menu">
-                <Link href={`/showcase/showcase-dark`}>
-                  <a className="dropdown-item">Carrasuel</a>
-                </Link>
-                <Link href={`/showcase4/showcase4-dark`}>
-                  <a className="dropdown-item">Carousel 2</a>
-                </Link>
-                <Link href={`/showcase3/showcase3-dark`}>
-                  <a className="dropdown-item">Circle</a>
-                </Link>
-                <Link href={`/works/works-dark`}>
-                  <a className="dropdown-item">Portfolio de Trabajo</a>
-                </Link>
-                <Link href={`/works2/works2-dark`}>
-                  <a className="dropdown-item">Portfolio Filtrado</a>
-                </Link>
-                <Link href={`/works3/works3-dark`}>
-                  <a className="dropdown-item">Portfolio Galeria</a>
-                </Link>
-              </div>
+            <li className="nav-item">
+              <Link href={`/works/works-dark`}>
+                <a className="nav-link">Nuestros proyectos</a>
+              </Link>
             </li>
             <li className="nav-item">
               <Link href={`/contact/contact-dark`}>
